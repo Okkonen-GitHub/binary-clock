cce0a5e28f5ebf721c601402f46801afb2f42785
finbows <finbows@users.noreply.github.com>
2021-06-17 17:41:36 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,50 +2,48 @@
 
 const binaryClock = () => {
     const date = new Date()
-    const hrs = splitTime(date.getHours())
-    const min = splitTime(date.getMinutes())
-    const sec = splitTime(date.getSeconds())
+    const hrs = splitDec(date.getHours())
+    const mins = splitDec(date.getMinutes())
+    const secs = splitDec(date.getSeconds())
 
-//paint the clock
-
-    paintClock(hrs.b0, 'b0', 'hours')
-    paintClock(hrs.b1, 'b1', 'hours')
-    paintClock(min.b0, 'b0', 'minutes')
-    paintClock(min.b1, 'b1', 'minutes')
-    paintClock(sec.b0, 'b0', 'seconds')
-    paintClock(sec.b1, 'b1', 'seconds')
+    paintClock(hrs[0], 'b0', 'hours')
+    paintClock(hrs[1], 'b1', 'hours')
+    paintClock(mins[0], 'b0', 'minutes')
+    paintClock(mins[1], 'b1', 'minutes')
+    paintClock(secs[0], 'b0', 'seconds')
+    paintClock(secs[1], 'b1', 'seconds')
 }
 
-const toBinary = (number) => Number(number).toString(2)
-
-const splitTime = (time) => {
-    const no = time.toString().split("")
-    if (no.length === 1) {
-        no = [0,no[0]]
-    }
-
-    return {
-        b0: toBinary(no[0]),
-        b1: toBinary(no[1])
-
-    }
-}
-
-const paintClock = (binTime,ninche,category) => {
-    binTime.split("").reverse().forEach((bin,index) => {
+const paintClock = (binary, ninche, category) => {
+    binary.split("").reverse().forEach((bin, index) => {
         const dom = Array.from(document.querySelectorAll(`.${category} .bin span.${ninche}`)).reverse()
-        console.log(dom)
-        if(bin === 1) {
+        if (bin === "1") {
             const light = dom[index]
-            light.classList += "light"
+            light.classList += " light"
         }
-    })
+    });
 }
 
 const clearClock = () => {
-    document.querySelectorAll(".light").forEach(light => light.classList.remove("light"))
+    document.querySelectorAll('.light').forEach(light => light.classList.remove('light'))
 }
+
+const toBin = (number) => {
+    return Number(number).toString(2)
+}
+
+const splitDec = (number) => {
+    let no = number.toString().split("")
+    if (no.length === 1) no = [0, no[0]]
+    return {
+        0: toBin(no[0]),
+        1: toBin(no[1])
+    }
+}
+
+// const run = async () => {
 setInterval(() => {
     clearClock()
     binaryClock()
 }, 1000)
+// }
